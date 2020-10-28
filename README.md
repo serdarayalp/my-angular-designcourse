873e89eb96729d962ae08724bc83546ba0d71fde
Serdar Ayalp <serdarayalp@googlemail.com>
2020-10-28 16:06:39 +0100

    first commit

--- README.md
+++ README.md
@@ -25,3 +25,8 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+*****************************************************************
+
+Mehr: https://coursetro.com/posts/code/174/Angular-8-Tutorial-&-Crash-Course
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+})
+export class ListComponent implements OnInit {
+  brews: Object;
+
+  constructor(private _http: HttpService) {}
+
+  // The service returns an observable, which means we can subscribe to it within the 
+  // component. In the return, we can pass the data to our brews object.
+  ngOnInit() {
+    this._http.getBeer().subscribe((data) => {
+      this.brews = data;
+      console.log(this.brews);
+    });
+  }
+}
